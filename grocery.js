d8cca1bd5e02ba8c83b30b8520a8d92ccd346a0a
jblaising <jblaising@gmail.com>
2015-07-28 22:07:43 -0500

    added functionality for the inStock and groceryLists

--- grocery.js
+++ grocery.js
@@ -1,7 +1,7 @@
 //grocery.js
 
 
-initLocalStorage();
+
 /*console.log(JSON.parse(localStorage.getItem('recipeList')));
 //initLocalStorage();
 var test = JSON.parse(localStorage.getItem('recipeList'));
@@ -11,6 +11,11 @@ test = localStorage.getItem('groceryList');
 console.log(test);
 console.log(test.split(","));
 console.log(test.toString());*/
+
+var groceryListHolder = document.getElementById("to-buy");
+var inStockListHolder = document.getElementById("in-stock");
+var ingredientsListHolder = document.getElementById("ingredients");
+var recipeListHolder = document.getElementById("recipes");
 
 //set default values in localStorage
 var defaultValues = function () {
@@ -39,14 +44,140 @@ var initLocalStorage = function(){
     //create default elements in local storage
 		defaultValues();
 	}
-    //load elements into the page
-	console.log("localStorage intact, loading page");
+	console.log("localStorage intact");
 };
 
 //load page from localStorage
-  //cycle through localStorage
+var loadPage = function() {
+  
+   //grocery list
+	loadList(createBasicListItem, localStorage.getItem('groceryList').split(","));
+   //instock
+   //ingredients
+   //recipes
     //create new taskitem for each element
     //append each one where it needs to go
+	
+}
+
+var loadList = function(createListItems, lables) {
+	for(var i = 0; i < lables.length; i++) {
+		var listItem = createListItems(lables[i]);
+		groceryListHolder.appendChild(listItem);  //<--------
+		bindTaskEvents(listItem, itemBought);
+	}
+	
+}
+
+//mark an item as bought and append it to the instock list
+var itemBought = function() {
+	console.log("item marked bought");
+	//append item to #in-stock
+	var listItem = this.parentNode;
+	inStockListHolder.appendChild(listItem);
+	bindTaskEvents(listItem, itemOutOfStock);
+	
+	//need remove item for localStorage GroceryList
+	//and add to localStorage instock
+}
+
+var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
+	console.log("bind list item events");
+    //select taskListItem's children
+    var checkBox = taskListItem.querySelector("input[type=checkbox]");
+    var editButton = taskListItem.querySelector("button.edit");
+    var deleteButton = taskListItem.querySelector("button.delete");
+	
+      //bind editTask to edit button
+    editButton.onclick = editItem;
+  
+        //bind deleteTask to delete button
+    deleteButton.onclick = deleteItem;
+  
+        //bind checkBoxEventHandler to the check box
+    checkBox.onchange = checkBoxEventHandler;
+}
+
+var editItem = function () {
+	console.log("edit task");             ///<----- edit to reflect changes in local storage
+  
+	  var listItem = this.parentNode;
+  
+	  var editInput = listItem.querySelector("input[type=text]");
+	  var label = listItem.querySelector("label");
+  
+	    // if the class of the parent is .editMode
+	  var containsClass = listItem.classList.contains("editMode");
+  
+	  if(containsClass) {
+	    // switch from .editMode
+	    // label text become the input's value
+	    label.innerText = editInput.value;
+	  } else{
+	    // switch to .editMode
+	    // input value becomes the labels text
+	    editInput.value = label.innerText;
+	  }
+	    // Toggle .editMode on the listItem
+	    listItem.classList.toggle("editMode");
+}
+
+var deleteItem = function () {
+	console.log("delete task...");
+	  // remove parent list item from the ul
+	  var listItem = this.parentNode;
+	  var ul = listItem.parentNode;
+  
+	  //remove the parent list item from the ul
+	  ul.removeChild(listItem);
+}
+
+//mark item as out of stock append to grocery list
+var itemOutOfStock = function() {
+	console.log("item marked out of stock");
+	var listItem = this.parentNode;
+	groceryListHolder.appendChild(listItem);
+	bindTaskEvents(listItem, itemBought);
+	
+	//need remove item for localStorage instock
+	//and add to localStorage groceryList
+}
+
+
+//new Grocery List Item
+var createBasicListItem = function(labelString, type) {
+	//create list item
+	var listItem = document.createElement("li");
+	//input checkbox
+	var checkBox = document.createElement("input");
+  // label
+    var label = document.createElement("label");
+    // input (text)
+    var editInput = document.createElement("input"); //text
+    // button.edit
+    var editButton = document.createElement("button");
+    // button.delete
+    var deleteButton = document.createElement("button");
+	
+	//modify each element
+	checkBox.type = "checkbox";
+	editInput.type = "text";
+	editButton.textContent = "Edit";
+	editButton.className = "edit";
+	deleteButton.textContent = "Delete";
+	deleteButton.className = "delete";
+	label.textContent = labelString;
+	
+	//append everything to the li
+	listItem.appendChild(checkBox);
+	listItem.appendChild(label);
+	listItem.appendChild(editInput);
+	listItem.appendChild(editButton);
+	listItem.appendChild(deleteButton);
+  
+	return listItem;
+}
+
 
 
 
@@ -95,3 +226,6 @@ var initLocalStorage = function(){
 
 
 //populate grocery list
+
+initLocalStorage();
+loadPage();
